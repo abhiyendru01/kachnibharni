992339c0e68266ac8ab1f00cb6f2dfafbf4c6334
Rahul Raj <rahul.r0644@gmail.com>
2024-12-04 03:37:07 +0530

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(), // Tailwind CSS integration
+    react(),    // React integration for using React components
+  ],
 });
